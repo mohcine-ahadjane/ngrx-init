fa21a301ad0d52c37c8c72806ed1df910bec433c
mohcine-ahadjane <mohcine-ahadjane@gmail.com>
2024-08-17 12:04:14 +0100

    ngrx key concepts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { counterReducer } from './states/counter/counter.reducer';
+import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({ count: counterReducer }),
+  ]
 };
